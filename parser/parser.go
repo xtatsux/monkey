376bde8fff08601e7f264b1cfdd8e6afcb5a9b7a
tatsu <phys.tatsu@gmail.com>
2020-03-17 00:41:34 +0900

    2.6.8 前置演算子を実装

--- parser/parser.go
+++ parser/parser.go
@@ -40,6 +40,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registertPrefix(token.IDENT, p.parseIdentifier)
 	p.registertPrefix(token.INT, p.parseIntegerLiteral)
+	p.registertPrefix(token.BANG, p.parsePrefixExpression)
+	p.registertPrefix(token.MINUS, p.parsePrefixExpression)
 
 	// curTokenとpeekTokenの2つを読み込む
 	p.nextToken()
@@ -162,9 +164,15 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) parseExpression(precedence int) ast.Expression {
 	prefix := p.prefixParseFns[p.curToken.Type]
 	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
 		return nil
 	}
 	leftExp := prefix()
@@ -188,4 +196,17 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 
 	lit.Value = value
 	return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
 }
